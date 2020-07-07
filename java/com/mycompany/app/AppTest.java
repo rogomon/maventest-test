31b86891849ef2558192e51f0097eaadc3afdb7e
Rod <rogomon.a@gmail.com>
2020-07-07 15:53:37 -0300

    1st 070620 maventest-test

---
+++ java/com/mycompany/app/AppTest.java
@@ -0,0 +1,28 @@
+package com.mycompany.app;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+  /**
+     * Rigorous Test :-)
+     */
+
+public class AppTest
+{
+	
+	@Test
+    public void shouldAnswerWithTrue()
+    {
+		assertTrue( true );
+    }	
+        
+    	
+    public static void main(String[] args)
+    {
+    	System.out.println("test");
+    }
+}
